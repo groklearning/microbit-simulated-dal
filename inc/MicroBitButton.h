f3e3872b03f37a1af43a1dee3a11ea72cf774012
Jim Mussared <jim.mussared@gmail.com>
2016-06-07 15:18:49 +1000

    Disable the real DAL's debouncing. It adds way too much latency.

--- inc/MicroBitButton.h
+++ inc/MicroBitButton.h
@@ -1,1 +1,10 @@
-../microbit-dal/inc/MicroBitButton.h
+#include "../microbit-dal/inc/MicroBitButton.h"
+
+// Replace button thresholds. We don't need debouncing.
+#undef MICROBIT_BUTTON_SIGMA_MAX
+#undef MICROBIT_BUTTON_SIGMA_THRESH_HI
+#undef MICROBIT_BUTTON_SIGMA_THRESH_LO
+
+#define MICROBIT_BUTTON_SIGMA_MAX               2
+#define MICROBIT_BUTTON_SIGMA_THRESH_HI         1
+#define MICROBIT_BUTTON_SIGMA_THRESH_LO         1
