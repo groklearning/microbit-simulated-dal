b372c4d031e93f34e01a2939fd35c272c60125d6
ta-grok <114370610+ta-grok@users.noreply.github.com>
2022-12-14 10:56:35 +1100

    ENG-1076 remove debug flag

--- targets/x86-linux-native-32bit/CMake/toolchain.cmake
+++ targets/x86-linux-native-32bit/CMake/toolchain.cmake
@@ -4,7 +4,7 @@ cmake_minimum_required(VERSION 2.8)
 
 # default to C99
 set(CMAKE_C_FLAGS "-std=c11 -Wno-int-to-pointer-cast -Wno-pointer-to-int-cast -Wno-int-conversion -Wno-incompatible-pointer-types -m32" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "-std=gnu++11 -Wno-int-to-pointer-cast -m32 -g" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-std=gnu++11 -Wno-int-to-pointer-cast -m32" CACHE STRING "")
 set(CMAKE_ASM_COMPILER_ARG1 "-m32")
 
 # check that we are actually running on Linux, if we're not then we may pull in
